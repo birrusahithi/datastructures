35492b4a9721cd456f4af358079901c2abfc237c
Sahithi Birru <106225654+birrusahithi@users.noreply.github.com>
2022-07-08 19:01:36 -0700

    Reverse String

---
+++ Strings/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+        public void reverseString(char[] s) {
+            int left =0;
+            int right = s.length-1;
+            while(left<right)  {
+                char c = s[left];
+                s[left] = s[right];
+                s[right]= c;
+                left++;
+                right--;
+            }
+        }
+    }
+
