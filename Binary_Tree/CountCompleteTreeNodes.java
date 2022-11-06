3a865ce92f878a4cd311788d74e8957143a2be6b
birrusahithi <birrusahithi@gmail.com>
2022-11-06 00:33:56 -0700

    Count all the node values

---
+++ Binary_Tree/CountCompleteTreeNodes.java
@@ -0,0 +1,23 @@
+package Binary_Tree;
+/*222. Count Complete Tree Nodes
+        Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+        According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+        Design an algorithm that runs in less than O(n) time complexity.
+
+        Input: root = [1,2,3,4,5,6]
+        Output: 6*/
+
+public class CountCompleteTreeNodes {
+    int count =0;
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        count++;
+        countNodes(root.left);
+        countNodes(root.right);
+        return count;
+    }
+}
